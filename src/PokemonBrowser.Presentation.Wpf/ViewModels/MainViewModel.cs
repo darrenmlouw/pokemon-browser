abefc409344759ba90380d059eed12c876475795
Darren <darrenmlouw@gmail.com>
2026-01-31 01:26:21 +0200

    Sets up initial project structure
    Initializes the project structure for the Pokemon Browser
    application using Clean Architecture principles with domain,
    application, infrastructure, presentation and tests projects.
    
    This structure facilitates separation of concerns,
    testability, and maintainability.

---
+++ src/PokemonBrowser.Presentation.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PokemonBrowser.Presentation.Wpf.Mvvm;
+
+namespace PokemonBrowser.Presentation.Wpf.ViewModels;
+
+public sealed class MainViewModel : ObservableObject
+{
+
+    public MainViewModel()
+    {}
+
+}
