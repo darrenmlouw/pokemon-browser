e0092300fa56295369a2189aa563f417744032d1
Darren Louw <darrenmlouw@gmail.com>
2026-01-31 02:36:14 +0200

    feat(wpf): bootstrap DI host

--- src/PokemonBrowser.Presentation.Wpf/App.xaml.cs
+++ src/PokemonBrowser.Presentation.Wpf/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PokemonBrowser.Infrastructure;
 using PokemonBrowser.Presentation.Wpf.ViewModels;
 
 namespace PokemonBrowser.Presentation.Wpf
@@ -14,16 +16,23 @@ namespace PokemonBrowser.Presentation.Wpf
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+
             _host = Host.CreateDefaultBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
+                    logging.AddFilter("Microsoft.Extensions.Http", LogLevel.Warning);
+                    logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Warning);
+                    logging.AddDebug();
+                })
                 .ConfigureServices(services =>
                 {
+                    services.AddInfrastructure();
+
                     services.AddSingleton<MainViewModel>();
                     services.AddSingleton<MainWindow>();
-
-                    // TODO: register Application services and Infrastructure implementations here.
-                    // Example:
-                    // services.AddApplication();
-                    // services.AddInfrastructure();
                 })
                 .Build();
 
@@ -32,8 +41,6 @@ namespace PokemonBrowser.Presentation.Wpf
             var mainWindow = _host.Services.GetRequiredService<MainWindow>();
             MainWindow = mainWindow;
             mainWindow.Show();
-
-            base.OnStartup(e);
         }
 
         protected override void OnExit(ExitEventArgs e)
